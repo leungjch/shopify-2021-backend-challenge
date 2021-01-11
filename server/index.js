cf7cab3917d24dc9cbc368100d307b8e130a4ce7
Justin <justin.leung.ch@gmail.com>
2021-01-10 23:59:22 -0500

    Edit server port for heroku

--- server/index.js
+++ server/index.js
@@ -197,7 +197,7 @@ app.get('/get_images', function (req, res) {
 
 
 // For production build on Heroku
-if (process.env.NODE_ENV === "production"{
+if (process.env.NODE_ENV === "production"){
     app.use(express.static("build"));
     app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
